b976b1259931efc3f30b44a9341bb2af27f57a15
anderson <anercabre@misena.edu.co>
2017-06-16 17:49:42 -0500

    ultimo comit
    gracias

--- controller/Horas_extras.php
+++ controller/Horas_extras.php
@@ -19,7 +19,7 @@
 			try {
 				Horas_Extras::registrarcliente($Gerente_Proyecto, $Nombre_Cliente, $Hora_Inicio, $Hora_Fin, $Horas_Laboradas);
 				echo "<script>alert('el registro de ".$Gerente_Proyecto."con exito');
-						 self.location.href='../view/menu/Consultar_Cliente.php';
+						 self.location.href='../sections/view/Consultar_Cliente.php#/';
 					</script>";
 			} catch (Exception $e) {
 
@@ -41,7 +41,8 @@
 			try {
 				Horas_Extras::Modificar_cliente($Gerente_Proyecto, $Nombre_Cliente, $Hora_Inicio, $Hora_Fin, $Horas_Laboradas, $actualizar);
 				echo "<script>alert('esta actualizado  ".$Gerente_Proyecto." con exito');
-						 self.location.href='../sections/Actualizar_cliente.php';
+						 self.location.href='../sections/view/Actualizar_cliente.php#/';
+
 					</script>";
 			} catch (Exception $e) {
 				echo "Se ha producido un error" .$e;
@@ -55,50 +56,13 @@
 			$Empresa_Contratante=$_POST['Empresa_Contratante'];
 
 			try {
-
 					Horas_Extras::RegistrarRecargo($Fecha,$Cantidad_Horas,$Comentario,$Empresa_Contratante,$Descripcion,$Descripcion1);
-
 				echo "<script>alert('el registro de ".$Fecha." con exito');
-						 self.location.href='../sections/Consulta_Analista.php';
+						 self.location.href='../sections/view/Consulta_Analista.php#/';
 					</script>";
 			} catch (Exception $e) {
 				echo "Se ha producido un error" .$e;
 			}
 		break;
-
-		// case 'Extras':
-		// 	try {
-		// 		Horas_Extras::Extras($Fecha, $Cantidad_Horas);
-		// 		echo "<script>alert('el registro de ".$Cantidad_Horas." con exito');
-		// 				 self.location.href='../sections/Consulta_Analista.php';
-		// 			</script>";
-		// 	} catch (Exception $e) {
-		// 		echo "Se ha producido un error" .$e;
-		// 	}
-		// break;
-
-	// 	case 'temporal':
-	//
-	// 	$Proyecto=$_POST['Proyecto'];
-	// 	$Fecha=$_POST['Fecha'];
-	// 	$Analista= $_POST['Analista'];
-	// 	$Actividad_Rh = $_POST['Actividad_Rh'];
-	// 	$Actividad=$_POST['Actividad'];
-	// 	$Comentario=$_POST['Comentario'];
-	// 	$Horas= $_POST['Horas'];
-	// 	$Empresa_Contrata = $_POST['Empresa_Contrata'];
-	// 	$ciudad=$_POST['ciudad'];
-	// 	$Extras=$_POST['Extras'];
-	//
-	// 	try {
-	// 		Horas_Extras::temporal($Proyecto,$Fecha,$Analista,$Actividad_Rh,$Actividad,$Comentario,$Horas,$Empresa_Contrata,$ciudad,$Extras);
-	// 		echo "<script>alert('el registro de ".$Fecha." con exito');
-	// 				 self.location.href='../sections/importar_archivo.php';
-	// 			</script>";
-	// 	} catch (Exception $e) {
-	// 		echo "Se ha producido un error" .$e;
-	// 	}
-	// break;
-
 }
  ?>
